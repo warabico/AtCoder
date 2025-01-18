9f4b0e02c191f10d0a91ffc50daf2eb13d612bba
warabico <121906002+warabico@users.noreply.github.com>
2025-01-18 19:32:36 +0900

    ABC383 D

--- abc/abc383/D/main.cpp
+++ abc/abc383/D/main.cpp
@@ -1,96 +1,160 @@
+/* includes *******************************************************************/
 #include <algorithm>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <list>
 #include <map>
+#include <queue>
+#include <set>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 
 /* namespaces *****************************************************************/
 using namespace std;
 
-/* macro definitions **********************************************************/
-#define FOR(i,a,b) for(long long (i)=(a); (i)<b; (i)++)
-#define REP(i,n)   FOR(i,0,n)
-#define MIN_V(v)   *min_element(v.begin(), v.end())
-#define MAX_V(v)   *max_element(v.begin(), v.end())
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
 
-#define YES        "Yes"
-#define NO         "No"
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
 
-/* type definitions ***********************************************************/
-typedef long long LL;
-typedef unsigned long long ULL;
+const double PI  = 3.14159265358979323846;
 
-/* library definitions ********************************************************/
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        ll n;
 
-/* variable definitions *******************************************************/
-LL n;
-vector<LL> primes;
-LL cnt;
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
 
-/* methods ********************************************************************/
-void input()
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
 {
     cin >> n;
 }
 
-void solve()
+void Solver::solve()
 {
-    bool isPrime;
-    cnt = 0;
-    for(int i=2; i<=n/2; i++)
-    {
-        isPrime = true;
-        REP(j,primes.size())
-        {
-            if( i % primes[j] == 0 )
-            {
-                isPrime = false;
+    ll ans = 0;
+
+    ll m = 1;
+    while( m*m <= n ) { m++; }
+    m--;
+
+    vll primes;
+    vector<bool> isprime(m, true);
+    isprime[0] = false;
+    isprime[1] = false;
+    for( ll i=2; i<m; i++ ) {
+        if( isprime[i] ) {
+            primes.push_back(i);
+            for( ll j=i*2; j<m; j+=i ) {
+                isprime[j] = false;
+            }
+        }
+    }
+
+    for( ll p: primes ) {
+        if( p*p*p*p > n ) {
+            break;
+        }
+
+        if( p < 100 ) {
+            if( p*p*p*p*p*p*p*p <= n ) {
+                ans++;
+            }
+        }
+        for( ll q: primes ) {
+            if( q <= p ) {
+                continue;
+            }
+
+            if( p*p*q*q <= n ) {
+                ans++;
+            } else {
                 break;
             }
         }
-        if(isPrime)
-        {
-            primes.push_back(i);
-        }
     }
-    REP(i, primes.size())
+    ss << ans << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    T element;
+    for( int i = 0; i < size; i++ )
     {
-        cout << primes[i] << endl;
-        for( LL j=i; j<primes.size(); j++ )
-        {
-            if( i == j )
-            {
-                if( primes[i] * primes[i] * primes[i] * primes[i] * primes[i] * primes[i] * primes[i] * primes[i] <= n )
-                {
-                    cnt++;
-                }
-            }
-            else
-            {
-                if( primes[i] * primes[i] * primes[j] * primes[j] <= n )
-                {
-                    cnt++;
-                }
-            }
-        }
+        cin >> element;
+        v.push_back(element);
     }
 }
 
-void output()
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
 {
-    cout << cnt << endl;
+    T element;
+    for( int i = 0; i < rows; i++ )
+    {
+        v.push_back(vector<T>());
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> element;
+            v[i].push_back(element);
+        }
+    }
 }
 
 /* main ***********************************************************************/
 int main()
 {
-    input();
-    solve();
-    output();
+    Solver solver;
+    solver.run();
     return 0;
 }
-
-/* libraries ******************************************************************/
-
