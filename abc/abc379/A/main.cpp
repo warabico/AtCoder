ff494a8d16b075cffaff91435bb659bca770da3b
warabico <121906002+warabico@users.noreply.github.com>
2025-01-20 22:16:54 +0900

    ABC379

---
+++ abc/abc379/A/main.cpp
@@ -0,0 +1,119 @@
+/* includes *******************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+/* namespaces *****************************************************************/
+using namespace std;
+
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
+
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
+
+const double PI  = 3.14159265358979323846;
+
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        string s;
+
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
+
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
+{
+    cin >> s;
+}
+
+void Solver::solve()
+{
+    ss << s[1] << s[2] << s[0] << " " << s[2] << s[0] << s[1] << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    T element;
+    for( int i = 0; i < size; i++ )
+    {
+        cin >> element;
+        v.push_back(element);
+    }
+}
+
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
+{
+    T element;
+    for( int i = 0; i < rows; i++ )
+    {
+        v.push_back(vector<T>());
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> element;
+            v[i].push_back(element);
+        }
+    }
+}
+
+/* main ***********************************************************************/
+int main()
+{
+    Solver solver;
+    solver.run();
+    return 0;
+}
---
+++ abc/abc379/C/main.cpp
@@ -0,0 +1,145 @@
+/* includes *******************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+/* namespaces *****************************************************************/
+using namespace std;
+
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
+
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
+
+const double PI  = 3.14159265358979323846;
+
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        ll n, m;
+        vector<pair<ll, ll>> xa;
+
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
+
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
+{
+    cin >> n >> m;
+    xa.assign(m, {0, 0});
+    for( int i=0; i<m; i++ ) {
+        cin >> xa[i].first;
+    }
+    for( int i=0; i<m; i++ ) {
+        cin >> xa[i].second;
+    }
+}
+
+void Solver::solve()
+{
+    ll sum = 0;
+    ll sum_idx = 0;
+
+    sort( xa.begin(), xa.end() );
+
+    for( int i=0; i<m; i++ ) {
+        if( sum < xa[i].first - 1 ) {
+            ss << "-1" << endl;
+            return;
+        }
+        sum += xa[i].second;
+        sum_idx += xa[i].second * xa[i].first;
+    }
+    if( sum != n ) {
+        ss << "-1" << endl;
+        return;
+    }
+    ss << n * (n + 1) / 2 - sum_idx << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    T element;
+    for( int i = 0; i < size; i++ )
+    {
+        cin >> element;
+        v.push_back(element);
+    }
+}
+
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
+{
+    T element;
+    for( int i = 0; i < rows; i++ )
+    {
+        v.push_back(vector<T>());
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> element;
+            v[i].push_back(element);
+        }
+    }
+}
+
+/* main ***********************************************************************/
+int main()
+{
+    Solver solver;
+    solver.run();
+    return 0;
+}
