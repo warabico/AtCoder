ec8377b708cf3bfa78c0aaee137b7ee0984e1f89
warabico <121906002+warabico@users.noreply.github.com>
2025-02-01 10:19:16 +0900

    Typical90 046

---
+++ typical90/046/main.cpp
@@ -0,0 +1,163 @@
+/* includes *******************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+/* namespaces *****************************************************************/
+using namespace std;
+
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
+
+/* Macro Definitions **********************************************************/
+#define rep(i,n) for(ll i=0; i<n; i++)
+
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
+const string SP  = " ";
+
+const double PI  = 3.14159265358979323846;
+
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        ll n;
+        vll a, b, c;
+
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
+
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+        template <typename T> void chmax(T& a, T b);
+        template <typename T> void chmin(T& a, T b);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
+{
+    cin >> n;
+    cin_v(a,n);
+    cin_v(b,n);
+    cin_v(c,n);
+}
+
+void Solver::solve()
+{
+    ll ans;
+    vll cntA(46, 0);
+    vll cntB(46, 0);
+    vll cntC(46, 0);
+
+    rep(i,n) {
+        ll modA = a[i] % 46;
+        ll modB = b[i] % 46;
+        ll modC = c[i] % 46;
+        cntA[modA]++;
+        cntB[modB]++;
+        cntC[modC]++;
+    }
+
+    ans = 0;
+    rep(i,46) {
+        rep(j,46) {
+            int k = ( 92 - (i+j) ) % 46;
+            ans += cntA[i] * cntB[j] * cntC[k];
+        }
+    }
+
+    ss << ans << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    v.assign(n, T{});
+    for( int i = 0; i < size; i++ )
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
+{
+    v.assign(rows, vector<T>(cols, T{}));
+    for( int i = 0; i < rows; i++ )
+    {
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> v[i][j];
+        }
+    }
+}
+
+template <typename T>
+void Solver::chmax(T& a, T b) {
+    if( a < b ) {
+        a = b;
+    }
+}
+
+template <typename T>
+void Solver::chmin(T& a, T b) {
+    if( a > b ) {
+        a = b;
+    }
+}
+
+/* main ***********************************************************************/
+int main()
+{
+    Solver solver;
+    solver.run();
+    return 0;
+}
