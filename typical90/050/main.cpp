e05821955e4f3d8b3b75f0e40476e1a62b9914d6
warabico <121906002+warabico@users.noreply.github.com>
2025-02-02 10:12:39 +0900

    Typical90 050

---
+++ typical90/050/main.cpp
@@ -0,0 +1,146 @@
+/* includes *******************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+/* namespaces *****************************************************************/
+using namespace std;
+
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
+
+/* Macro Definitions **********************************************************/
+#define rep(i,n) for(ll i=0; i<n; i++)
+
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
+
+const double PI  = 3.14159265358979323846;
+
+const ll MOD = 1000000007;
+
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        ll n,l;
+
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
+
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+        template <typename T> void chmax(T& a, T b);
+        template <typename T> void chmin(T& a, T b);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
+{
+    cin >> n >> l;
+}
+
+void Solver::solve()
+{
+    vll dp(n+1,0);
+    
+    dp[0] = 1;
+    for(ll i=1; i<=n; i++) {
+        if(i < l) dp[i] = dp[i-1];
+        else dp[i] = (dp[i-1] + dp[i-l]) % MOD;
+    }
+
+    ss << dp[n] << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    v.assign(size, T{});
+    for( int i = 0; i < size; i++ )
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
+{
+    v.assign(rows, vector<T>(cols, T{}));
+    for( int i = 0; i < rows; i++ )
+    {
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> v[i][j];
+        }
+    }
+}
+
+template <typename T>
+void Solver::chmax(T& a, T b) {
+    if( a < b ) {
+        a = b;
+    }
+}
+
+template <typename T>
+void Solver::chmin(T& a, T b) {
+    if( a > b ) {
+        a = b;
+    }
+}
+
+/* main ***********************************************************************/
+int main()
+{
+    Solver solver;
+    solver.run();
+    return 0;
+}
