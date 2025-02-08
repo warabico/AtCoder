db27da14293d25aef71f53df253d9999003e4e52
warabico <121906002+warabico@users.noreply.github.com>
2025-02-09 08:32:06 +0900

    Typical90 076

---
+++ typical90/076/main.cpp
@@ -0,0 +1,169 @@
+/* includes *******************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+/* namespaces *****************************************************************/
+using namespace std;
+
+/* Type Definitions ***********************************************************/
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vs   = vector<string>;
+using vvi  = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs  = vector<vector<string>>;
+
+/* Macro Definitions **********************************************************/
+#define rep(i,n) for(ll i=0; i<n; i++)
+
+/* Const Definitions **********************************************************/
+const string YES = "YES";
+const string NO  = "NO";
+const string Yes = "Yes";
+const string No  = "No";
+
+const double PI  = 3.14159265358979323846;
+
+/* Class Definition ***********************************************************/
+class Solver {
+    private:
+        /* String Stream ******************************************************/
+        stringstream ss;
+        ll n;
+        vll a;
+        ll sum;
+
+        /* User Dedfined Variables ========================================== */
+        /* ================================================================== */
+
+        /* Common Functions */
+        void input();
+        void solve();
+        void output();
+
+        /* User Dedfined Functions ========================================== */
+        /* ================================================================== */
+
+    public:
+        /* Common Functions */
+        void run();
+
+        /* Libraries */
+        template <typename T> void cin_v(vector<T>& v, int size);
+        template <typename T> void cin_vv(vector<vector<T>>& v, int rows, int cols);
+        template <typename T> void chmax(T& a, T b);
+        template <typename T> void chmin(T& a, T b);
+};
+
+/* Methods ================================================================== */
+void Solver::input()
+{
+    cin >> n;
+    sum = 0;
+    a.assign(n,0);
+    rep(i,n) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    rep(i,n) {
+        a.push_back(a[i]);
+    }
+}
+
+void Solver::solve()
+{
+    if( sum % 10 != 0 ) {
+        ss << No << endl;
+        return;
+    }
+    else {
+        ll target = sum / 10;
+        ll temp_sum = 0;
+        ll left = 0;
+        ll right = 0;
+        rep(i, a.size()) {
+            temp_sum += a[i];
+            right = i;
+            while( temp_sum > target || ( (right - left) > n ) ) {
+                temp_sum -= a[left];
+                left++;
+            }
+            if( temp_sum == target ) {
+                ss << Yes << endl;
+                return;
+            }
+        }
+    }
+    ss << No << endl;
+}
+/* ========================================================================== */
+
+/* Common Methods and Libraries ***********************************************/
+void Solver::run()
+{
+    input();
+    solve();
+    output();
+}
+
+void Solver::output()
+{
+    cout << ss.str();
+}
+
+template <typename T>
+void Solver::cin_v(vector<T>& v, int size)
+{
+    v.assign(size, T{});
+    for( int i = 0; i < size; i++ )
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename T>
+void Solver::cin_vv(vector<vector<T>>& v, int rows, int cols)
+{
+    v.assign(rows, vector<T>(cols, T{}));
+    for( int i = 0; i < rows; i++ )
+    {
+        for( int j = 0; j < cols; j++ )
+        {
+            cin >> v[i][j];
+        }
+    }
+}
+
+template <typename T>
+void Solver::chmax(T& a, T b) {
+    if( a < b ) {
+        a = b;
+    }
+}
+
+template <typename T>
+void Solver::chmin(T& a, T b) {
+    if( a > b ) {
+        a = b;
+    }
+}
+
+/* main ***********************************************************************/
+int main()
+{
+    Solver solver;
+    solver.run();
+    return 0;
+}
